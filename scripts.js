f01a18fa8a0d6e32d1ce8131197246f328bda639
Alternatykw <126626738+Alternatykw@users.noreply.github.com>
2024-02-04 17:36:26 +0100

    fixed prolly idk

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
+plclicked=0;
+enclicked=1;
 
-function colourFunction(){      
-    var x = document.getElementById("colourpick");
+function colourFunctionpl(){      
+    var x = document.getElementById("colourpickpl");
+    var currentVal = x.value;
+    document.documentElement.style.setProperty('--colours', currentVal);
+}
+function colourFunctionen(){      
+    var x = document.getElementById("colourpicken");
     var currentVal = x.value;
     document.documentElement.style.setProperty('--colours', currentVal);
 }
@@ -11,8 +18,18 @@ function changeLanguage(language) {
     if (language === 'en') {
         englishContent.style.display = 'flex';
         polishContent.style.display = 'none';
+        if(enclicked==0){
+            document.getElementById("colourpicken").value=document.getElementById("colourpickpl").value;
+            enclicked=1;
+            plclicked=0;
+        }
     } else if (language === 'pl') {
         englishContent.style.display = 'none';
         polishContent.style.display = 'flex';
+        if(plclicked==0){
+            document.getElementById("colourpickpl").value=document.getElementById("colourpicken").value;
+            plclicked=1;
+            enclicked=0;
+        }
     }
 }
